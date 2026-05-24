3c74a6b979076826ceddd8df019709604eaff47e
LewisM-Code <lewismichaelmcdonald@gmail.com>
2026-05-24 23:49:18 +0100

    Problems 186 and 197

---
+++ 197_Rising_Temperature.sql
@@ -0,0 +1,62 @@
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
+
+SELECT
+    id
+FROM (
+    SELECT
+        id,
+        recorddate,
+        temperature AS current_temp,
+        LAG(temperature,1) OVER(ORDER BY recorddate ASC) AS prev_temp,
+        LAG(recorddate,1) OVER(ORDER BY recorddate ASC) AS prev_date
+    FROM
+        weather
+) AS daily_comparison
+WHERE
+    current_temp > prev_temp AND
+    prev_date = recorddate - 1
